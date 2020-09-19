0f35a648372ce29ca267984ffff2f8dbbbbfe0f3
CamilleVG <u0953582@eng.utah.edu>
2020-09-18 21:56:16 -0600

    Should now have deleted files that arnt compiling.

--- Spreadsheet/FormulaTests/FormulaTests.cs
+++ Spreadsheet/FormulaTests/FormulaTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpreadsheetUtilities;
 using System.Collections.Generic;
